1e3b4dbecbd36183082e250d8ebe5158d7d4eb24
michaeljensen821 <msjensen821@cox.net>
2019-01-22 19:04:58 -0500

    this is hello git command

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Michael Jensen
+ *        Date: January 22nd 2019
+ * Description: git lab assinment
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "hello git!!" << endl;
+    return 0;
+}// main
